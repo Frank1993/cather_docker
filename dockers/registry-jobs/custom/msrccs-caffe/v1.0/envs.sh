2f820e8d65f247faab932097e144565379f5cfd8
Ehsan Azarnasab <ehazar@microsoft.com>
2018-02-05 18:37:25 +0000

    Merged PR 1458: Caffe SSH to monitor file truncate.
    Caffe SSH to monitor file truncate.
    Download a smaller NLTK data (for smaller container image) and for all the users

--- dockers/registry-jobs/custom/msrccs-caffe/v1.0/envs.sh
+++ dockers/registry-jobs/custom/msrccs-caffe/v1.0/envs.sh
@@ -3,3 +3,4 @@
 export CAFFE_ROOT=${CAFFE_ROOT:-/opt/caffe}
 export PYCAFFE_ROOT=${PYCAFFE_ROOT:-$CAFFE_ROOT/python}
 export PYTHONPATH=${PYTHONPATH:-$PYCAFFE_ROOT:$PYTHONPATH}
+export NLTK_DATA=${NLTK_DATA:-/home/job/nltk_data}
--- dockers/registry-jobs/custom/msrccs-caffe/v1.0/scripts/caffe_ssh.py
+++ dockers/registry-jobs/custom/msrccs-caffe/v1.0/scripts/caffe_ssh.py
@@ -53,64 +53,83 @@ def main():
     with cwd(data_path):
         s = time.time()
         f = None
+        st_ino = None
+        st_size = 0
         max_iter = None
         progress = 1
         loss = 1
-        stdout_idx = 0
-        # Pipe caffe output like `| ~/stdout.txt` for Philly to see the progress
-        base_path = os.environ.get('PHILLY_HOME', os.environ.get('HOME', '/tmp'))
+        # Pipe caffe output like `| tee ~/stdout.txt` for Philly to see the progress
+        base_path = os.path.expanduser('~')
         stdout_file = os.path.join(base_path, 'stdout.txt')
-        while os.path.exists(stdout_file):
-            stdout_idx += 1
-            stdout_file = os.path.join(base_path, 'stdout_%d.txt' % stdout_idx)
-        exit_file = os.path.join(base_path, 'exit')
+        # ignore if any leftover from previous try
+        if os.path.exists(stdout_file):
+            # noinspection PyBroadException
+            try:
+                f = open(stdout_file)
+                f.seek(0, os.SEEK_END)
+            except:
+                print("stdout removed")
         # `touch ~/exit` to exit the job
+        exit_file = os.path.join(base_path, 'exit')
         while not os.path.exists(exit_file):
-            if os.path.exists(stdout_file):
-                if not f:
-                    # noinspection PyBroadException
-                    try:
-                        f = open(stdout_file)
-                    except:
-                        print("stdout removed")
+            # noinspection PyBroadException
+            try:
+                st = os.stat(stdout_file)
+            except:
+                st = None
+            # if appended to log file
+            if st and (not st_ino or st.st_ino == st_ino) and st.st_size >= st_size:
+                st_size = st.st_size
+                st_ino = st.st_ino
             else:
+                st = None
+            if not st:
                 if f:
                     f.close()
                 f = None
-            if f:
-                # if there is any stdout, Tee it
-                line = f.readline()
-                if not line:
-                    time.sleep(1)
-                    if time.time() - s > 60:
-                        s = time.time()
-                        print("PROGRESS: {}%".format(progress))
-                        print("EVALERR: {}%".format(loss))
-                    continue
-                line = line.strip()
-                print(line)
-                if not line:
-                    continue
-                if not max_iter:
-                    # Another chance to find max_iter
-                    m = re.match(max_iter_pattern, line)
-                    if m:
-                        max_iter = int(m.group('MAX_ITER'))
-                if not max_iter:
-                    continue
-                m = re.match(iter_loss_pattern, line)
-                if m:
-                    progress = float(m.group('ITERATION')) / max_iter * 100
-                    loss = float(m.group('LOSS'))
+                st_ino = None
+                st_size = 0
+                # Keep alive with no log file
+                time.sleep(10)
+                if time.time() - s > 10:
+                    s = time.time()
                     print("PROGRESS: {}%".format(progress))
                     print("EVALERR: {}%".format(loss))
                 continue
-
-            time.sleep(10)
-            if time.time() - s > 10:
-                s = time.time()
+            if not f:
+                # noinspection PyBroadException
+                try:
+                    f = open(stdout_file)
+                except Exception as e:
+                    print('error opening the log file: {}'.format(e))
+                    continue
+            # if there is any stdout, Tee it
+            line = f.readline()
+            if not line:
+                # Keep alive
+                time.sleep(10)
+                if time.time() - s > 60:
+                    s = time.time()
+                    print("PROGRESS: {}%".format(progress))
+                    print("EVALERR: {}%".format(loss))
+                continue
+            line = line.strip()
+            print(line)
+            if not line:
+                continue
+            # find max_iter if changed
+            m = re.match(max_iter_pattern, line)
+            if m:
+                max_iter = int(m.group('MAX_ITER'))
+            if not max_iter:
+                continue
+            m = re.match(iter_loss_pattern, line)
+            if m:
+                progress = float(m.group('ITERATION')) / max_iter * 100
+                loss = float(m.group('LOSS'))
                 print("PROGRESS: {}%".format(progress))
                 print("EVALERR: {}%".format(loss))
+
         if f:
             f.close()
     print("PROGRESS: {}%".format(100))
