39ca95bfba01d49522bd2a25be6733ca69c2f274
Kairu Li <v-kairli@microsoft.com>
2019-06-27 08:05:04 +0000

    Merged PR 7287: edit openmpi edition

--- dockers/registry-jobs/custom/pytorch/pytorch0.4.1-py36-cuda9-mpi-hvd/openmpi_install.sh
+++ dockers/registry-jobs/custom/pytorch/pytorch0.4.1-py36-cuda9-mpi-hvd/openmpi_install.sh
@@ -1,6 +1,6 @@
 tar -xvf /openmpi-4.0.0.tar.gz -C /
 ls /
 cd /openmpi-4.0.0/
-sh configure --prefix=/usr/local
-make all install
-ldconfig
+sh configure --prefix=/usr/local/openmpi/
+make -j8
+make install -j8
